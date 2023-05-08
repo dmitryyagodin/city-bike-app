3f23b32a4b8163199ee66686a21b48aab7131055
Dmitry Yagodin <dmitry.yagodin@crasman.fi>
2023-05-08 13:07:02 +0300

    test: add page title test, station page by id test and filtering tests

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,27 +1,50 @@
-// const pages = [
-//   'http://app:3000/',
-//   'http://app:3000/rides',
-//   'http://app:3000/stations'
-// ];
+const pages = [
+  {
+    name: 'home',
+    url: 'http://app:3000/'
+  },
+  {
+    name: 'rides',
+    url: 'http://app:3000/rides'
+  },
+  {
+    name: 'on-map',
+    url: 'http://app:3000/stations/on-map'
+  },
+  {
+    name: 'stations',
+    url: 'http://app:3000/stations'
+  },
+  {
+    name: 'station/1',
+    url: 'http://app:3000/stations/1'
+  }
+];
 
-// describe('E2E Test', () => {
-//   beforeEach(() => {
-//     cy.visit('/');
-//   });
-// });
+describe('Pages should be available and include h1', () => {
 
-describe('The home page should be accessible', () => {
-  it('Visits the home page', () => {
-    cy.visit('http://app:3000/');
-    cy.get('h1').should('exist');
-    cy.log('h1 exists');
+  pages.forEach(page => {
+    it(`Page: "${page.name}" should be available`, () => {
+      cy.visit(page.url);
+      cy.get('h1').should('exist');
+    });
+
+    it('Should include a non-empty h1 tag', () => {
+      cy.visit(page.url);
+      cy.get('h1').should('exist').then($el => expect($el).not.to.be.empty);
+    });
+
+    it('Should include a non-empty title tag', () => {
+      cy.visit(page.url);
+      cy.get('title').should('exist').then($el => expect($el).not.to.be.empty);
+    });
   });
 });
+
 
 describe('The rides page should include <table> with 6 <th> cols', () => {
   it('Finds the table with 6 columns', () => {
     cy.visit('http://app:3000/rides');
-    cy.get('h1').should('exist');
     cy.get('table').within(() => {
       cy.get('td')
         .should('have.length', 300) // 50 rows x 6 colums
@@ -42,11 +65,59 @@ describe('The stations page should include non-empty ul>li*10', () => {
   });
 });
 
-describe('The stations page should include filter intput field', () => {
+
+/**
+ * Test stations filter 
+ */
+describe('The stations filter should work', () => {
   it('Finds the filter intput field', () => {
     cy.visit('http://app:3000/stations');
-    cy.get('input[name="filter"]').should('exist');
+    cy.get('input[name="filter"]').type('diakoniapuisto');
+
+    cy.get('main ul').within(() => {
+      cy.get('li')
+        .should('have.length', 1)
+        .then(($el) => expect($el).to.have.text('Diakoniapuisto'));
+    });
   });
+});
+
+/**
+ * Test rides filter
+ */
+describe('The rides filter should work', () => {
+  it('Finds "By station name" details tag', () => {
+    cy.visit('http://app:3000/rides');
+    
+    // initially the filter is hidden behind the details tag
+    cy.get('input[name="departure_station"]').should('not.be.visible');
+    
+    // find the filter and click to open
+    cy.get('details h3')
+      .first()
+      .within(($el) => expect($el).to.have.text('By station name'))
+      .click();
+    
+    // the filter should now be visible
+    cy.get('input[name="departure_station"]').should('be.visible');
+    
+    // type a station name 
+    cy.get('input[name="departure_station"]').type('Linnanmäki');
+
+    // control the request
+    cy.intercept('/\/api/rides/query').as('apiCheck');
+
+    // submit the filter request
+    cy.get('button[type="submit"]').contains('Filter').click();
+    
+    // wait for data load
+    cy.wait('@apiCheck');
+    
+    // check for correct filter
+    cy.get('td[data-label="From Station:"]')
+      .should('have.length', 50)
+      .each(($el) => expect($el).to.have.text('Linnanmäki'));
+    });
 });
 
 export {};
