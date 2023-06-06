68fab2b30c6216f1a02ef5e671bcb006908335d5
Dmitry Yagodin <dmitry.yagodin@crasman.fi>
2023-06-06 08:59:49 +0300

    fix: improve accessibility with logo icon link aria-label text

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
         <nav>
           <ul>
             <li>
-              <Link href="/">
+              <Link href="/" aria-label="bike logo">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="40"
